f7667e6a09f9d8a9ce1cf4147b3acee78382a5a1
irynaprofile <105556785+irynaprofile@users.noreply.github.com>
2022-11-28 19:31:37 -0600

    showing astronauts

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 // TODO: add code here
+window.addEventListener("load", function() {
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then( function(response) {
+        
+        response.json().then( function(json) {
+           console.log(json);
+            const div = document.getElementById("container");
+            let astronautHTML = "";
+
+            for (let i = 0; i < json.length; i++) {
+                astronautHTML += `
+                    <div class="astronaut">
+                        <div class="bio ${json[i].active ? "active" : "" }">
+                            <h3>${json[i].firstName} ${json[i].lastName}</h3>
+                            <ul>
+                                <li>Hours in space: ${json[i].hoursInSpace}</li>
+                                <li>Active:${json[i].active}</li>
+                                <li>Skills: ${json[i].skills}</li>
+                            </ul>
+                        </div>
+                        <img class="avatar" src="${json[i].picture}">
+                    </div>`
+            }
+
+            astronautHTML += `
+            <div><h3>Astronaut Count: ${json.length}</h3></div> `
+
+            div.innerHTML = astronautHTML;
+        });
+     });
+    });
